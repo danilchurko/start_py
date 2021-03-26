23f53f1fd66d0641b83e62fc520a58e61e9f27c5
Danil Churko <55187974+danilchurko@users.noreply.github.com>
2021-03-26 13:32:25 +0200

    Initial commit

---
+++ hw/hw_1.py
@@ -0,0 +1,35 @@
+# 3
+# from __future__ import braces
+# joke)
+
+# import this
+# zen python
+
+# import antigravity
+# python cartoon
+
+# import __hello__
+# simple "Hello world"
+
+# 4
+from art import *
+
+tprint("DANILO", font="block", chr_ignore=True)
+
+# 5
+a = input("1 -->")
+b = input("2 -->")
+
+print(a + " " + b)
+
+a = int(a)
+b = int(b)
+
+print(a + b)
+
+print(a // b)
+
+if a > b:
+    print(a % b)
+else:
+    print(b % a)
